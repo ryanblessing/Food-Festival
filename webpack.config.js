a9cb47a0c021018b07c749183aeb62315d9ee679
Ryan Blessing <ryanblessing20@yahoo.com>
2021-08-17 13:02:05 -0500

    hard lesson, going to have to go back through that to fully understand all the 'wiring' that goes with multiple data points

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,56 @@
-const path = require('path');
+
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
+
 
 //basic webpack configuration needs 3 properties- entry, output and mode
 // 1st. entry is root of the bundle and the beginning of the dependency graph
 // 2nd. - Output- bundle that code, and output that bundled code to a folder we specify
 // 3rd. mode- default will be production, but in this example we want it to run of development mode
-module.exports = {
-entry: './assets/js/script.js', 
-output: {
-    path: path.resolve(__dirname, 'dist'),
-filename: 'main.bundle.js'
-},
-plugins: [
-    new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-    }),
-],
-mode: 'development'
+const config = {
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [{
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                        name(file) {
+                            return '[path][name].[ext]'
+                        },
+                        publicPath: function (url) {
+                            return url.replace('../', '/assets/')
+                        }
+                    }
+                },
+                //put the loader right after the 'use' {} and in between the [] 
+                {
+                    loader: 'image-webpack-loader'
+                }
+            ]
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outpus to a html file in the dist folder
+        })
+    ],
+    mode: 'development'
 };
+
+module.exports = config;
